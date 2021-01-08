32c4a2a5a2cba1a0ab2cf4612ea45bc99199c3e8
dhanush72 <dhanuraj7258@gmail.com>
2021-01-08 17:53:53 +0530

    seo meta tags added

--- src/components/Service/Service-Data.js
+++ src/components/Service/Service-Data.js
@@ -5,14 +5,14 @@ export const data = [
   {
     id: 1,
     icon: HyperLocal,
-    alt: "HyperLocal",
+    alt: "HyperLocal Icon",
     title: "Hyper Local",
     subtitle: "Delivery services within a small and medium geographical area.",
   },
   {
     id: 2,
     icon: Courier,
-    title: "Courier",
+    title: "Courier Icon",
     subtitle:
       "Door to Door pickup & delivery services to selected postal codes across India.",
   },
